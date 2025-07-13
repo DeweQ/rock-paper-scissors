a19e3b8a0a6be4d78e9febf9b07bc578d588f660
DeweQ <thedeweq@yandex.ru>
2025-07-13 16:08:11 +0300

    Add playRound function to game.js
    This function adds logic to play a round of game. It compares player's
    choices and decides on a winner of a round.

--- game.js
+++ game.js
@@ -1,4 +1,7 @@
 const hands = ["rock", "paper", "scissors"];
+let computerScore = 0;
+let humanScore = 0;
+console.log(hands.indexOf('rock'));
 
 function getComputerChoice() {
     let choice = getRandomNumber(0, 3);
@@ -7,6 +10,57 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     return prompt("Choose your hand! Type 'rock', 'paper' or 'scissors'.");
+}
+
+function playRound(humanChoice, computerChoice) {
+    let humanWin = () => {
+        console.log("You win!");
+        humanScore++;
+        return;
+    }
+    let computerWin = () => {
+        console.log("You lose!")
+        computerScore++;
+        return
+    }
+    let humanChoiceInsensitive = humanChoice.toLowerCase();
+    if (humanChoiceInsensitive === computerChoice) {
+        console.log("It's a draw!");
+        return;
+    }
+    switch (humanChoiceInsensitive) {
+        case "rock":
+            switch (computerChoice) {
+                case "paper":
+                    computerWin();
+                    break;
+                case "scissors":
+                    humanWin();
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    humanWin();
+                    break;
+                case "scissors":
+                    computerWin();
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "paper":
+                    humanWin();
+                    break;
+                case "rock":
+                    computerWin();
+                    break;
+            }
+            break;
+    }
+    // if (hands.indexOf(humanChoice) > hands.indexOf(computerChoice) $$
 }
 
 function getRandomNumber(min, max) {
