74a0a892fe332282d5af7cd06b23acd72ba5fd27
DeweQ <thedeweq@yandex.ru>
2025-07-13 15:37:25 +0300

    Add human player logic to game.js

--- game.js
+++ game.js
@@ -1,12 +1,16 @@
-const hands = ["rock","paper","scissors"];
+const hands = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-    let choice = getRandomNumber(0,3);    
+    let choice = getRandomNumber(0, 3);
     return hands[choice];
 }
 
+function getHumanChoice() {
+    return prompt("Choose your hand! Type 'rock', 'paper' or 'scissors'.");
+}
+
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random()* (max)) + min;
+    return Math.floor(Math.random() * (max)) + min;
 }
 // for (let i = 1;i<10;i++) 
 //     console.log(getComputerChoice());
